734b88cd383835718454b1a970a4c4aae1728ce5
xman <xman@localhost.localdomain>
2021-12-19 17:39:39 +0100

    Remove unwraps + make pyo3 usage dependent on the python feature

--- estim2b_lib/src/lib.rs
+++ estim2b_lib/src/lib.rs
@@ -1,17 +1,28 @@
+// TODO Workaround until https://github.com/PyO3/pyo3/issues/780 and https://github.com/PyO3/pyo3/issues/1003 is resolved
+#![feature(cfg_eval)]
+
 mod device;
 
 use serde::{Deserialize, Serialize};
 use std::fmt;
+use std::num::ParseIntError;
+use std::convert::Infallible;
 
 #[cfg(feature = "usb")]
 pub use device::usb_two_b::USBTwoB;
 #[cfg(feature = "virtual")]
 pub use device::virtual_two_b::VirtualTwoB;
 
+#[cfg(feature = "python")]
+use pyo3::prelude::pyclass;
+
 use num_enum::{IntoPrimitive, TryFromPrimitive, TryFromPrimitiveError};
-use pyo3::prelude::*;
 use strum::ParseError as StrumParserError;
 use strum_macros::{Display, EnumString, EnumVariantNames};
+
+
+
+
 
 #[derive(Debug, Display, Serialize, Deserialize)]
 pub enum TwoBError {
@@ -22,6 +33,19 @@ pub enum TwoBError {
 impl From<&str> for TwoBError {
     fn from(s: &str) -> TwoBError {
         TwoBError::ParserError(s.into())
+    }
+}
+
+impl From<ParseIntError> for TwoBError {
+    fn from(e: ParseIntError) -> TwoBError {
+        TwoBError::ParserError(format!("{}", e))
+    }
+}
+
+
+impl From<Infallible> for TwoBError {
+    fn from(e: Infallible) -> TwoBError {
+        TwoBError::ParserError(format!("{}", e))
     }
 }
 
@@ -37,7 +61,7 @@ impl From<StrumParserError> for TwoBError {
     }
 }
 
-#[pyclass]
+#[cfg_attr(feature="python", pyclass)]
 #[repr(u8)]
 #[derive(
     Clone,
@@ -74,7 +98,7 @@ pub enum TwoBMode {
     Training,
 }
 
-#[pyclass]
+#[cfg_attr(feature="python", pyclass)]
 #[repr(u8)]
 #[derive(
     Clone,
@@ -97,7 +121,7 @@ pub enum TwoBBias {
     MAX,
 }
 
-#[pyclass]
+#[cfg_attr(feature="python", pyclass)]
 #[repr(u8)]
 #[derive(
     Clone,
@@ -119,7 +143,7 @@ pub enum TwoBMap {
     C,
 }
 
-#[pyclass]
+#[cfg_attr(feature="python", pyclass)]
 #[repr(u8)]
 #[derive(
     Clone,
@@ -142,7 +166,7 @@ pub enum TwoBRamp {
     X4,
 }
 
-#[pyclass]
+#[cfg_attr(feature="python", pyclass)]
 #[repr(u8)]
 #[derive(
     Clone,
@@ -167,7 +191,7 @@ pub enum TwoBWarp {
     X32,
 }
 
-#[pyclass]
+#[cfg_attr(feature="python", pyclass)]
 #[derive(
     Clone, Copy, Display, Debug, Eq, PartialEq, EnumVariantNames, EnumString, Serialize, Deserialize,
 )]
@@ -200,7 +224,7 @@ impl TryFrom<String> for TwoBPower {
     }
 }
 
-#[pyclass]
+#[cfg_attr(feature="python", pyclass)]
 #[derive(
     Clone, Copy, Display, Debug, Eq, PartialEq, EnumVariantNames, EnumString, Serialize, Deserialize,
 )]
@@ -223,32 +247,34 @@ impl TwoBChannel {
     }
 }
 
-#[pyclass]
+// TODO Workaround until https://github.com/PyO3/pyo3/issues/780 and https://github.com/PyO3/pyo3/issues/1003 is resolved
+#[cfg_eval]
+#[cfg_attr(feature="python", pyclass)]
 #[derive(Clone, Debug, Eq, PartialEq, Serialize, Deserialize)]
 pub struct TwoBState {
-    #[pyo3(get, set)]
+    #[cfg_attr(feature="python", pyo3(get, set))]
     pub mode: TwoBMode,
-    #[pyo3(get, set)]
+    #[cfg_attr(feature="python", pyo3(get, set))]
     pub channel_a: u8,
-    #[pyo3(get, set)]
+    #[cfg_attr(feature="python", pyo3(get, set))]
     pub channel_b: u8,
-    #[pyo3(get, set)]
+    #[cfg_attr(feature="python", pyo3(get, set))]
     pub channel_c: u8,
-    #[pyo3(get, set)]
+    #[cfg_attr(feature="python", pyo3(get, set))]
     pub channel_d: u8,
-    #[pyo3(get, set)]
+    #[cfg_attr(feature="python", pyo3(get, set))]
     pub power: TwoBPower,
-    #[pyo3(get, set)]
+    #[cfg_attr(feature="python", pyo3(get, set))]
     pub bias: TwoBBias,
-    #[pyo3(get, set)]
+    #[cfg_attr(feature="python", pyo3(get, set))]
     pub joined_channels: bool,
-    #[pyo3(get, set)]
+    #[cfg_attr(feature="python", pyo3(get, set))]
     pub map: TwoBMap,
-    #[pyo3(get, set)]
+    #[cfg_attr(feature="python", pyo3(get, set))]
     pub ramp: TwoBRamp,
-    #[pyo3(get, set)]
+    #[cfg_attr(feature="python", pyo3(get, set))]
     pub warp: TwoBWarp,
-    #[pyo3(get)]
+    #[cfg_attr(feature="python", pyo3(get, set))]
     pub battery: u16,
 }
 
@@ -257,28 +283,39 @@ impl TryFrom<&String> for TwoBState {
     fn try_from(s: &String) -> Result<Self, TwoBError> {
         macro_rules! get_value {
             ($x:ident) => {
-                $x.next().unwrap().parse().unwrap()
+                if let Some(value) = $x.next() {
+                    Ok(value.parse()?)
+                }
+                else {
+                    Err(TwoBError::ConnectionError("No value to parse".into()))
+                }
+
             };
             ($x:ident, $t:ty) => {
-                $x.next().unwrap().parse::<$t>().unwrap()
+                if let Some(value) = $x.next() {
+                    Ok(value.parse::<$t>()?)
+                }
+                else {
+                    Err(TwoBError::ConnectionError("No value to parse".into()))
+                }
             };
         }
 
         let mut split = s.split(':');
 
         Ok(TwoBState {
-            battery: get_value!(split),
-            channel_a: get_value!(split, u8) / 2,
-            channel_b: get_value!(split, u8) / 2,
-            channel_c: get_value!(split, u8) / 2,
-            channel_d: get_value!(split, u8) / 2,
-            mode: TwoBMode::try_from(get_value!(split, u8))?,
-            power: TwoBPower::try_from(get_value!(split, String))?,
-            bias: TwoBBias::try_from(get_value!(split, u8))?,
-            joined_channels: get_value!(split, u8) == 1,
-            map: TwoBMap::try_from(get_value!(split, u8))?,
-            warp: TwoBWarp::try_from(get_value!(split, u8))?,
-            ramp: TwoBRamp::try_from(get_value!(split, u8))?,
+            battery: get_value!(split)?,
+            channel_a: get_value!(split, u8)? / 2,
+            channel_b: get_value!(split, u8)? / 2,
+            channel_c: get_value!(split, u8)? / 2,
+            channel_d: get_value!(split, u8)? / 2,
+            mode: TwoBMode::try_from(get_value!(split, u8)?)?,
+            power: TwoBPower::try_from(get_value!(split, String)?)?,
+            bias: TwoBBias::try_from(get_value!(split, u8)?)?,
+            joined_channels: get_value!(split, u8)? == 1,
+            map: TwoBMap::try_from(get_value!(split, u8)?)?,
+            warp: TwoBWarp::try_from(get_value!(split, u8)?)?,
+            ramp: TwoBRamp::try_from(get_value!(split, u8)?)?,
         })
     }
 }
